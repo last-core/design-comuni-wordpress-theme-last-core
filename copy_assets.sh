865dcc529789a092731a17d2a72a3731d3a23ccb
Lorenzo Castro <l.castro@enway.it>
2023-07-28 08:51:18 +0000

    add alpinejs

--- copy_assets.sh
+++ copy_assets.sh
@@ -2,3 +2,5 @@
 
 cp ./node_modules/bootstrap-italia/dist/css/bootstrap-italia-comuni.min.css ./assets/css/bootstrap-italia.min.css
 cp ./node_modules/bootstrap-italia/dist/js/bootstrap-italia.bundle.min.js ./assets/js/bootstrap-italia.bundle.min.js
+mkdir ./assets/js/alpinejs/
+cp ./node_modules/alpinejs/dist/cdn.min.js ./assets/js/alpinejs/cdn.min.js
