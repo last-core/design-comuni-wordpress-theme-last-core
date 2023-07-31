f3da288dde11da57bdb649a3ba6f4e044d60d0a7
Lorenzo Castro <l.castro@enway.it>
2023-07-31 15:07:33 +0000

    enable archive on notizie

--- inc/admin/tassonomie/tassonomia_tipi_notizia.php
+++ inc/admin/tassonomie/tassonomia_tipi_notizia.php
@@ -3,29 +3,30 @@
 /**
  * Definisce la tassonomia Tipi di Notizia
  */
-add_action( 'init', 'dci_register_taxonomy_tipi_notizia', -10 );
-function dci_register_taxonomy_tipi_notizia() {
+add_action('init', 'dci_register_taxonomy_tipi_notizia', -10);
+function dci_register_taxonomy_tipi_notizia()
+{
 
     $labels = array(
-        'name'              => _x( 'Tipi di Notizia', 'taxonomy general name', 'design_comuni_italia' ),
-        'singular_name'     => _x( 'Tipo di Notizia', 'taxonomy singular name', 'design_comuni_italia' ),
-        'search_items'      => __( 'Cerca Tipo di Notizia', 'design_comuni_italia' ),
-        'all_items'         => __( 'Tutti i Tipi di Notizia ', 'design_comuni_italia' ),
-        'edit_item'         => __( 'Modifica il Tipo di Notizia', 'design_comuni_italia' ),
-        'update_item'       => __( 'Aggiorna il Tipo di Notizia', 'design_comuni_italia' ),
-        'add_new_item'      => __( 'Aggiungi un Tipo di Notizia', 'design_comuni_italia' ),
-        'new_item_name'     => __( 'Nuovo Tipo di Notizia', 'design_comuni_italia' ),
-        'menu_name'         => __( 'Tipi di Notizia', 'design_comuni_italia' ),
+        'name'              => _x('Tipi di Notizia', 'taxonomy general name', 'design_comuni_italia'),
+        'singular_name'     => _x('Tipo di Notizia', 'taxonomy singular name', 'design_comuni_italia'),
+        'search_items'      => __('Cerca Tipo di Notizia', 'design_comuni_italia'),
+        'all_items'         => __('Tutti i Tipi di Notizia ', 'design_comuni_italia'),
+        'edit_item'         => __('Modifica il Tipo di Notizia', 'design_comuni_italia'),
+        'update_item'       => __('Aggiorna il Tipo di Notizia', 'design_comuni_italia'),
+        'add_new_item'      => __('Aggiungi un Tipo di Notizia', 'design_comuni_italia'),
+        'new_item_name'     => __('Nuovo Tipo di Notizia', 'design_comuni_italia'),
+        'menu_name'         => __('Tipi di Notizia', 'design_comuni_italia'),
     );
 
     $args = array(
         'hierarchical'      => true,
         'labels'            => $labels,
-        'public'            => false, //enable to get term archive page
+        'public'            => true, //enable to get term archive page
         'show_ui'           => true,
         'show_admin_column' => true,
         'query_var'         => true,
-        'has_archive'           => false,    //archive page
+        'has_archive'           => true,    //archive page
         //'rewrite'           => array( 'slug' => 'novita' ),
         'capabilities'      => array(
             'manage_terms'  => 'manage_tipi_notizia',
@@ -35,5 +36,5 @@ function dci_register_taxonomy_tipi_notizia() {
         )
     );
 
-    register_taxonomy( 'tipi_notizia', array( 'notizia' ), $args );
+    register_taxonomy('tipi_notizia', array('notizia'), $args);
 }
