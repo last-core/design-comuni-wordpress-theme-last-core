584dfb3a065ae38a6b6a1acf956c4f3d50378aa5
Lorenzo Castro <l.castro@enway.it>
2023-08-01 08:08:57 +0000

    add default style replace

--- caraffa_style.sh
+++ caraffa_style.sh
@@ -1,6 +1,3 @@
-mv ./assets/css/bootstrap-italia.min.css ./assets/css/bootstrap-italia.min.bak.css
-mv ./assets/css/comuni.css  ./assets/css/comuni.bak.css
-mv ./assets/img/evidenza-header.png ./assets/img/evidenza-header.bak.png
 cp ./overrides/caraffa/bootstrap-italia.min.css ./assets/css/bootstrap-italia.min.css
 cp ./overrides/caraffa/comuni.css ./assets/css/comuni.css
 cp ./overrides/caraffa/evidenza-header.png ./assets/img/evidenza-header.png
