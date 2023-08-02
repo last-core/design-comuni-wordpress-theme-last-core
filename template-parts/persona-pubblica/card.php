c114b875d0969cd40037e97369bffdad3eed6689
Lorenzo Castro <l.castro@enway.it>
2023-08-02 10:39:24 +0000

    fix: cards persona pubblica

--- template-parts/persona-pubblica/card.php
+++ template-parts/persona-pubblica/card.php
@@ -5,51 +5,49 @@ $img = dci_get_meta('foto', $prefix, $post->ID);
 $descrizione = dci_get_meta('descrizione_breve', $prefix, $post->ID);
 $incarichi = dci_get_meta('incarichi', $prefix, $post->ID);
 ?>
-<div class="card-wrapper px-0 card-overlapping card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
-    <div class="card card-teaser card-teaser-image card-flex no-after rounded shadow">
-        <div class="card-image-wrapper with-read-more pb-5">
-            <div class="card-body p-4">
-                <h3 class="card-title text-paragraph-medium u-grey-light"> <?php echo $post->post_title ?> </h3>
-                <div class="text-paragraph-card u-grey-light m-0">
-                    <div class="mt-1"> </div>
-                    <?php if ($descrizione !== '') { ?>
-                        <div class="mt-1">
-                            <p><?php echo $descrizione; ?></p>
-                        </div>
-                    <?php
-                    }
-                    if (is_array($incarichi) && count($incarichi)) {
-                    ?>
-                        <div class="mt-1">
-                            <ul class="list-unstyled">
-                                <?php
-                                $i_prefix = '_dci_incarico_';
-                                foreach ($incarichi as $_incarico) {
-                                    $incarico = get_post($_incarico);
-                                    $unita_organizzativa = dci_get_meta('unita_organizzativa', $i_prefix, $incarico->ID);
-                                ?>
-                                    <li>
-                                        <?php echo $incarico->post_title; ?>
-                                        <?php if ($unita_organizzativa !== '') {
-                                            $unita_organizzativa = get_post($unita_organizzativa);
-                                        ?>
-                                            presso <a href="<?php echo get_permalink($unita_organizzativa->ID); ?>"><?php echo $unita_organizzativa->post_title ?></a>
-                                        <?php } ?>
-                                    </li>
-                                <?php } ?>
-                            </ul>
-                        </div>
-                    <?php } ?>
-                </div>
+<div class="card card-teaser card-teaser-image card-flex no-after rounded shadow">
+    <div class="card-image-wrapper with-read-more pb-5">
+        <div class="card-body p-4">
+            <h3 class="card-title text-paragraph-medium u-grey-light"> <?php echo $post->post_title ?> </h3>
+            <div class="text-paragraph-card u-grey-light m-0">
+                <div class="mt-1"> </div>
+                <?php if ($descrizione !== '') { ?>
+                    <div class="mt-1">
+                        <p><?php echo $descrizione; ?></p>
+                    </div>
+                <?php
+                }
+                if (is_array($incarichi) && count($incarichi)) {
+                ?>
+                    <div class="mt-1">
+                        <ul class="list-unstyled">
+                            <?php
+                            $i_prefix = '_dci_incarico_';
+                            foreach ($incarichi as $_incarico) {
+                                $incarico = get_post($_incarico);
+                                $unita_organizzativa = dci_get_meta('unita_organizzativa', $i_prefix, $incarico->ID);
+                            ?>
+                                <li>
+                                    <?php echo $incarico->post_title; ?>
+                                    <?php if ($unita_organizzativa !== '') {
+                                        $unita_organizzativa = get_post($unita_organizzativa);
+                                    ?>
+                                        presso <a href="<?php echo get_permalink($unita_organizzativa->ID); ?>"><?php echo $unita_organizzativa->post_title ?></a>
+                                    <?php } ?>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                <?php } ?>
             </div>
-            <?php if ($img !== '') { ?>
-                <div class="card-image card-image-rounded pb-5">
-                    <?php dci_get_img($img) ?>
-                </div>
-            <?php } ?>
-            <a class="read-more ps-4" href="<?php echo get_permalink($post->ID); ?>"> <span class="text">Ulteriori dettagli</span> <svg class="icon">
-                    <use xlink:href="#it-arrow-right"></use>
-                </svg> </a>
         </div>
+        <?php if ($img !== '') { ?>
+            <div class="card-image card-image-rounded pb-5">
+                <?php dci_get_img($img) ?>
+            </div>
+        <?php } ?>
+        <a class="read-more ps-4" href="<?php echo get_permalink($post->ID); ?>"> <span class="text">Ulteriori dettagli</span> <svg class="icon">
+                <use xlink:href="#it-arrow-right"></use>
+            </svg> </a>
     </div>
 </div>
--- template-parts/persona-pubblica/cards-list.php
+++ template-parts/persona-pubblica/cards-list.php
@@ -2,11 +2,13 @@
 global $obj, $the_query, $load_posts, $load_card_type, $additional_filter;
 ?>
 <div class="container py-5">
-    <div id="load-more" class="row g-4">
-        <?php foreach ($posts as $post) {
-            $load_card_type = "persona-pubblica";
-            get_template_part("template-parts/persona-pubblica/card");
-        } ?>
+    <div class="row g-4">
+        <div id="load-more" class="card-wrapper px-0 card-overlapping card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
+            <?php foreach ($posts as $post) {
+                $load_card_type = "persona-pubblica";
+                get_template_part("template-parts/persona-pubblica/card");
+            } ?>
+        </div>
+        <?php get_template_part("template-parts/search/more-results"); ?>
     </div>
-    <?php get_template_part("template-parts/search/more-results"); ?>
 </div>
