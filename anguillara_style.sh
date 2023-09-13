775bbd482ac2d062bc6854cfd4b4c20f828affdd
Lorenzo Castro <l.castro@enway.it>
2023-09-13 10:21:49 +0000

    add anguillara style

---
+++ anguillara_style.sh
@@ -0,0 +1,3 @@
+cp ./overrides/anguillara/bootstrap-italia.min.css ./assets/css/bootstrap-italia.min.css
+cp ./overrides/anguillara/comuni.css ./assets/css/comuni.css
+cp ./overrides/anguillara/evidenza-header.png ./assets/img/evidenza-header.png
