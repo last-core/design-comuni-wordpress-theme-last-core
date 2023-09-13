ac6738552a34a4c55a93e0c765afe4fc111ec320
Lorenzo Castro <l.castro@enway.it>
2023-09-13 13:43:57 +0000

    link support for area riservata

--- template-parts/header/header-anon.php
+++ template-parts/header/header-anon.php
@@ -1,4 +1,9 @@
-<a class="btn btn-primary btn-icon btn-full" href="#" data-element="personal-area-login">
+<?php
+$area_riservata_link = dci_get_option("area_riservata_link", "header");
+
+?>
+
+<a class="btn btn-primary btn-icon btn-full" href="<?php echo $area_riservata_link ? $area_riservata_link : '#' ?>" data-element="personal-area-login">
     <span class="rounded-icon" aria-hidden="true">
         <svg class="icon icon-primary">
             <use xlink:href="#it-user"></use>
