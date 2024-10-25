c46d601be5e155f9bcbd7d15bbd107c30cd2fe8d
Lorenzo Castro <l.castro@enway.it>
2024-10-25 14:54:42 +0200

    contatti layout fix

--- single-servizio.php
+++ single-servizio.php
@@ -426,11 +426,11 @@ get_header();
                                 <h2 class="mb-3" id="contacts">Contatti</h2>
                                 <div class="row">
                                     <div class="col-12 col-md-8 col-lg-6 mb-30">
-                                        <div class="card-wrapper rounded h-auto mt-10">
-                                            <?php foreach ($contatti as $pc_id) {
-                                                get_template_part("template-parts/punto-contatto/card");
-                                            } ?>
-                                        </div>
+                                        <?php foreach ($contatti as $pc_id) { ?>
+                                            <div class="card-wrapper rounded h-auto mt-10">
+                                                <?php get_template_part("template-parts/punto-contatto/card"); ?>
+                                            </div>
+                                        <?php } ?>
                                     </div>
                                 </div>
                             <?php } ?>
